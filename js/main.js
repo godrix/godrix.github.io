1048dbc3d16cc1cc9b9210a7ee4a54cc274b90af
Carlos Godri <cggodri@gmail.com>
2017-01-20 13:42:20 -0200

    new template

---
+++ js/main.js
@@ -0,0 +1,31 @@
+var span = document.getElementById('txt');
+var contentxt = 'odriz://a';
+
+function writetxt(str, el) {
+    var char = str.split('').reverse();
+    var typer = setInterval(function () {
+        if (!char.length) return clearInterval(typer);
+        var next = char.pop();
+        el.innerHTML += next;
+    }, 200);
+}
+
+writetxt(contentxt, span);
+
+$('.header_menu a[href^="#"]').click(function (e) {
+    e.preventDefault();
+    var anchor = $(this).attr('href'),
+        menuHeight = $('.header').innerHeight(),
+        targetOffset = $(anchor).offset().top;
+    $('html, body').animate({
+        scrollTop: targetOffset - menuHeight
+    }, 500);
+    console.log(targetOffset);
+});
+
+$('.logo').click(function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: 0
+    }, 500)
+});
